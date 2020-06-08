14102ab78a6bf2d410e44367461de1049d695496
HamishLamond <62045373+HamishLamond@users.noreply.github.com>
2020-06-09 08:29:27 +1000

    Script Updates

---
+++ IoTBayPrototype/build/web/db/loadAccessLog.sql
@@ -0,0 +1,33 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Hamish Lamond
+ * Created: 08/06/2020
+ */
+
+INSERT INTO IOTBAY.ACCESSLOG
+    (CUSTOMERID, EVENTTYPE)
+    VALUES
+    (1, 'customerCreated'),
+    (2, 'customerCreated'),
+    (3, 'customerCreated'),
+    (4, 'customerCreated'),
+    (5, 'customerCreated'),
+    (6, 'customerCreated'),
+    (7, 'customerCreated'),
+    (8, 'customerCreated'),
+    (9, 'customerCreated'),
+    (10, 'customerCreated'),
+    (11, 'customerCreated'),
+    (12, 'customerCreated'),
+    (13, 'customerCreated'),
+    (14, 'customerCreated'),
+    (15, 'customerCreated'),
+    (16, 'customerCreated'),
+    (17, 'customerCreated'),
+    (18, 'customerCreated'),
+    (19, 'customerCreated'),
+    (20, 'customerCreated')
